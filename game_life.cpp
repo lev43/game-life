17a4e5127004896faca77bc2c64ed27ebf53d2bd
lev43 <68313120+lev43@users.noreply.github.com>
2020-11-19 18:28:30 +0300

    t

--- game_life.cpp
+++ game_life.cpp
@@ -131,7 +131,7 @@ int main(int argc, char *argv[]) {
 				int x=ceil(float(xev.xbutton.x)/(win_size-1)*size)-1, y=size-ceil(float(xev.xbutton.y)/(win_size-1)*size);
 				if(x==pX && y==pY)continue;
 				pX=x; pY=y;
-				dots[y][x]->setActive(xev.xmotion.state==272 || xev.xmotion.state==256 || xev.xmotion.state==258);
+				dots[y][x]->setActive(xev.xmotion.state==272 || xev.xmotion.state==256 || xev.xmotion.state==258 || xev.xmotion.state==8448 || xev.xmotion.state==8464);
 				dots[y][x]->updateActive();
 			}
 			if(xev.type==ButtonPress){
